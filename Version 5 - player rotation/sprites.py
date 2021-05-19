21cdccd6a311ada8ad0002e8b155eafd9ec5010a
shez2650 <62561531+shez2650@users.noreply.github.com>
2021-05-19 22:19:49 +0100

    yes

---
+++ Version 5 - player rotation/sprites.py
@@ -0,0 +1,81 @@
+import pygame, sys, random, os
+from pygame.locals import *
+from pygame.math import Vector2
+from tilemap import collide_hit_box
+from settings import *
+from sprites import *
+
+
+class Player(pygame.sprite.Sprite):
+    # Sprite for the player
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.player_img
+        self.rect = self.image.get_rect()
+        self.hit_box = PLAYER_HIT_BOX
+        self.hit_box.center = self.rect.center
+        self.vel = Vector2(0, 0)
+        self.pos = Vector2(x, y) * TILESIZE
+        self.rot = 0
+            
+    def get_keys(self):
+        self.rot_speed = 0
+        self.vel = Vector2(0, 0)
+        keys = pygame.key.get_pressed()
+        if keys[K_a] or keys[K_LEFT]:
+            self.rot_speed = PLAYER_ROT_SPEED
+        if keys[K_d] or keys[K_RIGHT]:
+            self.rot_speed = -PLAYER_ROT_SPEED
+        if keys[K_w] or keys[K_UP]:
+            self.vel = Vector2(PLAYER_SPEED, 0).rotate(-self.rot)
+        if keys[K_s] or keys[K_DOWN]:
+            self.vel = Vector2(-PLAYER_SPEED / 2, 0).rotate(-self.rot)
+    
+    def collide_with_walls(self, dir):
+        if dir == "x":
+            hits = pygame.sprite.spritecollide(self, self.game.walls, False, collide_hit_box)
+            if hits:
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.hit_box.width / 2
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right + self.hit_box.width / 2
+                self.vel.x = 0
+                self.hit_box.centerx = self.pos.x
+        if dir == "y":
+            hits = pygame.sprite.spritecollide(self, self.game.walls, False, collide_hit_box)
+            if hits:
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.hit_box.height / 2
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom + self.hit_box.width / 2
+                self.vel.y = 0
+                self.hit_box.centery = self.pos.y
+    
+    
+    def update(self):
+        self.get_keys()
+        self.rot = (self.rot + self.rot_speed * self.game.dt) % 360
+        self.image = pygame.transform.rotate(self.game.player_img, self.rot)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+        self.pos += self.vel * self.game.dt
+        self.hit_box.centerx = self.pos.x
+        self.collide_with_walls("x")
+        self.hit_box.centery = self.pos.y
+        self.collide_with_walls("y")
+        self.rect.center = self.hit_box.center
+        
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pygame.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
---
+++ Version 6 - mobs/main.py
@@ -0,0 +1,105 @@
+import pygame, sys, random
+from os import path
+from pygame.locals import *
+from settings import *
+from sprites import *
+from tilemap import *
+
+class Game():
+    def __init__(self):
+        # Initialize game window, etc
+        pygame.init()
+        pygame.mixer.init()
+        pygame.display.set_caption(TITLE)
+        pygame.key.set_repeat(300, 75)
+        self.running = True
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.load_data()
+    
+    def load_data(self):
+        self.map = Map(path.join(map_folder, "map3.txt"))
+        self.player_img = pygame.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
+        self.mob_img = pygame.image.load(path.join(img_folder, MOB_IMG)).convert_alpha()
+        self.wall_img = pygame.image.load(path.join(img_folder, WALL_IMG)).convert_alpha()
+        self.wall_img = pygame.transform.scale(self.wall_img, (TILESIZE, TILESIZE))
+    
+    def new(self):
+        # start a new game
+        self.all_sprites = pygame.sprite.Group()
+        self.walls = pygame.sprite.Group()
+        self.mobs = pygame.sprite.Group()
+        for y, tiles in enumerate(self.map.data):
+            for x, tile in enumerate(tiles):
+                if tile == "1":
+                    Wall(self, x, y)
+                if tile == "P":
+                    self.player = Player(self, x, y)
+                if tile == "M":
+                    Mob(self, x, y)
+        
+        self.camera = Camera(self.map.width, self.map.height)
+        
+        g.run()
+    
+    def run(self):
+        # Game Loop
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+    
+    def quit(self):
+        pygame.quit()
+        sys.exit()
+    
+    def update(self):
+        # Game Loop - update
+        self.all_sprites.update()
+        self.camera.update(self.player)
+    
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pygame.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+    
+        for y in range(0, HEIGHT, TILESIZE):
+            pygame.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+            
+    def draw(self):
+        # Game Loop - draw
+        
+        # Set caption to FPS
+        pygame.display.set_caption("{:.2f}".format(self.clock.get_fps()))
+        self.screen.fill(BGCOLOUR)
+        #self.draw_grid()
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
+        #pygame.draw.rect(self.screen, WHITE, self.player.hit_box, 2)
+        pygame.display.flip()
+    
+    def events(self):
+        # Game Loop - events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.quit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+    
+    def show_start_screen(self):
+        # Game start screen
+        pass
+    
+    def show_go_screen(self):
+        # Game over/continue screen
+        pass
+    
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
+    
+pygame.quit()
