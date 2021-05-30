c4b7e166f9d9264db7281469774de3c81b2c68a4
shez2650 <62561531+shez2650@users.noreply.github.com>
2021-05-30 17:23:17 +0100

    v11

---
+++ Version 11 - better mob movement/main.py
@@ -0,0 +1,166 @@
+import pygame, sys
+from os import path
+from pygame.locals import *
+from settings import *
+from sprites import *
+from tilemap import *
+
+# HUD functions
+def draw_player_health(surf, x, y, percent):
+    if percent < 0:
+        percent = 0
+    BAR_WIDTH = 100
+    BAR_HEIGHT = 20
+    fill = percent * BAR_WIDTH
+    outline_rect = pygame.Rect(x, y, BAR_WIDTH, BAR_HEIGHT)
+    fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
+    if percent < 60:
+        col = GREEN
+    elif percent < 30:
+        col = YELLOW
+    else:
+        col = RED
+    pygame.draw.rect(surf, col, fill_rect)
+    pygame.draw.rect(surf, WHITE, outline_rect, 2)
+
+class Game():
+    def __init__(self):
+        # Initialize game window, etc
+        pygame.init()
+        pygame.mixer.init()
+        pygame.display.set_caption(TITLE)
+        pygame.key.set_repeat(300, 75)
+        self.running = True
+        self.draw_debug = False
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.load_data()
+    
+    def load_data(self):
+        self.map = TileMap(path.join(map_folder, "game_map.tmx"))
+        self.map_img = self.map.make_map()
+        self.map_rect = self.map_img.get_rect()
+        self.player_img = pygame.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
+        self.bullet_img = pygame.image.load(path.join(img_folder, BULLET_IMG)).convert_alpha()
+        self.mob_img = pygame.image.load(path.join(img_folder, MOB_IMG)).convert_alpha()
+        self.wall_img = pygame.image.load(path.join(img_folder, WALL_IMG)).convert_alpha()
+        self.wall_img = pygame.transform.scale(self.wall_img, (TILESIZE, TILESIZE))
+    
+    def new(self):
+        # start a new game
+        self.all_sprites = pygame.sprite.Group()
+        self.walls = pygame.sprite.Group()
+        self.mobs = pygame.sprite.Group()
+        self.bullets = pygame.sprite.Group()
+        
+        # load the map
+        # for y, tiles in enumerate(self.map.data):
+        #     for x, tile in enumerate(tiles):
+        #         if tile == "1":
+        #             Wall(self, x, y)
+        #         if tile == "P":
+        #             self.player = Player(self, x, y)
+        #         if tile == "M":
+        #             Mob(self, x, y)
+        for tile_object in self.map.tmxdata.objects:
+            if tile_object.name == "Player":
+                self.player = Player(self, tile_object.x, tile_object.y)
+            if tile_object.name == "Mob":
+                Mob(self, tile_object.x, tile_object.y)
+            if tile_object.name == "Wall":
+                Obstacle(self, tile_object.x, tile_object.y, tile_object.width, tile_object.height)
+        self.camera = Camera(self.map.width, self.map.height)
+        
+        g.run()
+    
+    def run(self):
+        # Game Loop
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+    
+    def quit(self):
+        pygame.quit()
+        sys.exit()
+    
+    def update(self):
+        # Game Loop - update
+        self.all_sprites.update()
+        self.camera.update(self.player)
+        # mobs hit player
+        hits = pygame.sprite.spritecollide(self.player, self.mobs, False, collide_hit_box)
+        for hit in hits:
+            now = pygame.time.get_ticks()
+            if now - hit.last_hit > MOB_ATTACK_SPEED:
+                hit.last_hit = now
+                self.player.health -= MOB_DAMAGE
+                hit.vel = Vector2(0, 0)
+            if self.player.health <= 0:
+                self.playing = False
+        if hits:
+            self.player.pos += Vector2(MOB_KNOCKBACK, 0).rotate(-hits[0].rot)
+        
+        # bullets hit mobs
+        hits = pygame.sprite.groupcollide(self.mobs, self.bullets, False, True)
+        for hit in hits:
+            hit.health -= BULLET_DAMAGE
+            hit.vel = Vector2(0, 0)
+    
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pygame.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+    
+        for y in range(0, HEIGHT, TILESIZE):
+            pygame.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+            
+    def draw(self):
+        # Game Loop - draw
+        
+        # Set caption to FPS
+        pygame.display.set_caption("{:.2f}".format(self.clock.get_fps()))
+        # self.screen.fill(BGCOLOUR)
+        self.screen.blit(self.map_img, self.camera.apply_rect(self.map_rect))
+        #self.draw_grid()
+        for sprite in self.all_sprites:
+            if isinstance(sprite, Mob):
+                sprite.draw_health()
+            self.screen.blit(sprite.image, self.camera.apply_sprite(sprite))
+            if self.draw_debug:
+                pygame.draw.rect(self.screen, CYAN, self.camera.apply_rect(sprite.hit_box), 1)
+        if self.draw_debug:
+            for wall in self.walls:
+                pygame.draw.rect(self.screen, CYAN, self.camera.apply_rect(wall.hit_box), 1)
+        #pygame.draw.rect(self.screen, WHITE, self.player.hit_box, 2)
+        # HUD functions
+        draw_player_health(self.screen, 10, 10, self.player.health / PLAYER_HEALTH)
+        pygame.display.flip()
+    
+    def events(self):
+        # Game Loop - events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.quit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    self.playing = False
+                if event.key == K_h:
+                    self.draw_debug = not self.draw_debug
+    
+    def show_start_screen(self):
+        # Game start screen
+        pass
+    
+    def show_go_screen(self):
+        # Game over/continue screen
+        pass
+    
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
+    
+pygame.quit()
---
+++ Version 11 - better mob movement/sprites.py
@@ -0,0 +1,175 @@
+import pygame, math
+from pygame.locals import *
+from pygame.math import Vector2
+from tilemap import collide_hit_box
+from settings import *
+from random import uniform
+
+def sine_based_animation(n):
+    return -0.5 * (math.cos(math.pi * n) - 1)
+
+def collide_with_walls(sprite, group, dir):
+        if dir == "x":
+            hits = pygame.sprite.spritecollide(sprite, group, False, collide_hit_box)
+            if hits:
+                if hits[0].rect.centerx > sprite.hit_box.centerx:
+                    sprite.pos.x = hits[0].rect.left - sprite.hit_box.width / 2
+                if hits[0].rect.centerx < sprite.hit_box.centerx:
+                    sprite.pos.x = hits[0].rect.right + sprite.hit_box.width / 2
+                sprite.vel.x = 0
+                sprite.hit_box.centerx = sprite.pos.x
+        if dir == "y":
+            hits = pygame.sprite.spritecollide(sprite, group, False, collide_hit_box)
+            if hits:
+                if hits[0].rect.centery > sprite.hit_box.centery:
+                    sprite.pos.y = hits[0].rect.top - sprite.hit_box.height / 2
+                if hits[0].rect.centery < sprite.hit_box.centery:
+                    sprite.pos.y = hits[0].rect.bottom + sprite.hit_box.width / 2
+                sprite.vel.y = 0
+                sprite.hit_box.centery = sprite.pos.y
+
+class Player(pygame.sprite.Sprite):
+    # Sprite for the player
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.player_img
+        self.rect = self.image.get_rect()
+        self.hit_box = PLAYER_HIT_BOX
+        self.hit_box.center = self.rect.center
+        self.vel = Vector2(0, 0)
+        self.pos = Vector2(x, y)
+        self.rot = -90
+        self.last_shot = 0
+        self.health = PLAYER_HEALTH
+            
+    def get_keys(self):
+        self.rot_speed = 0
+        self.vel = Vector2(0, 0)
+        keys = pygame.key.get_pressed()
+        if keys[K_a] or keys[K_LEFT]:
+            self.rot_speed = PLAYER_ROT_SPEED
+        if keys[K_d] or keys[K_RIGHT]:
+            self.rot_speed = -PLAYER_ROT_SPEED
+        if keys[K_w] or keys[K_UP]:
+            self.vel = Vector2(PLAYER_SPEED, 0).rotate(-self.rot)
+        if keys[K_s] or keys[K_DOWN]:
+            self.vel = Vector2(-PLAYER_SPEED / 2, 0).rotate(-self.rot)
+        if keys[K_SPACE]:
+            now = pygame.time.get_ticks()
+            if now - self.last_shot > FIRE_RATE:
+                self.last_shot = now
+                dir = Vector2(1, 0).rotate(-self.rot)
+                pos = self.pos + BARREL_OFFSET.rotate(-self.rot)
+                Bullet(self.game, pos, dir)
+                self.vel = Vector2(-KICK_BACK, 0).rotate(-self.rot)
+    
+    def update(self):
+        self.get_keys()
+        self.rot = (self.rot + self.rot_speed * self.game.dt) % 360
+        self.image = pygame.transform.rotate(self.game.player_img, self.rot)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+        self.pos += self.vel * self.game.dt
+        self.hit_box.centerx = self.pos.x
+        collide_with_walls(self, self.game.walls, "x")
+        self.hit_box.centery = self.pos.y
+        collide_with_walls(self, self.game.walls, "y")
+        self.rect.center = self.hit_box.center
+        
+class Mob(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.mobs
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.mob_img
+        self.rect = self.image.get_rect()
+        self.hit_box = MOB_HIT_BOX.copy()
+        self.hit_box.center = self.rect.center
+        self.pos = Vector2(x, y)
+        self.vel = Vector2(0, 0)
+        self.acc = Vector2(0, 0) # Acceleration
+        self.rect.center = self.pos
+        self.rot = 0
+        self.health = MOB_HEALTH
+        self.last_hit = 0
+        
+    def update(self):
+        self.rot = (self.game.player.pos - self.pos).angle_to((Vector2(1, 0)))
+        self.image = pygame.transform.rotate(self.game.mob_img, self.rot)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+        self.acc = Vector2(MOB_SPEED, 0).rotate(-self.rot)
+        self.acc += self.vel * -1
+        self.vel += self.acc * self.game.dt
+        self.pos += self.vel * self.game.dt + 0.5 * self.acc * self.game.dt ** 2
+        self.hit_box.centerx = self.pos.x
+        collide_with_walls(self, self.game.walls, "x")
+        self.hit_box.centery = self.pos.y
+        collide_with_walls(self, self.game.walls, "y")
+        self.rect.center = self.hit_box.center
+        if self.health <= 0:
+            if self.game.player.health + 20 > PLAYER_HEALTH:
+                self.game.player.health = PLAYER_HEALTH
+            else:
+                self.game.player.health += 20
+            self.kill()
+    
+    def draw_health(self):
+        if self.health / MOB_HEALTH * 100 > 60:
+            col = GREEN
+        elif self.health / MOB_HEALTH * 100 > 30:
+            col = YELLOW
+        else:
+            col = RED
+        width = int(self.rect.width * self.health / MOB_HEALTH)
+        self.health_bar = pygame.Rect(0, 0, width, 7)
+        if self.health < MOB_HEALTH:
+            pygame.draw.rect(self.image, col, self.health_bar)
+        
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, game, pos, dir):
+        self.groups = game.all_sprites, game.bullets
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.bullet_img
+        self.rect = self.image.get_rect()
+        self.hit_box = self.rect
+        self.pos = Vector2(pos)
+        self.rect.center = pos
+        spread = uniform(-BULLET_SPREAD, BULLET_SPREAD)
+        self.vel = dir.rotate(spread) * BULLET_SPEED * self.game.dt
+        self.spawn_time = pygame.time.get_ticks()
+        
+    def update(self):
+        self.pos += self.vel
+        self.rect.center = self.pos
+        if pygame.sprite.spritecollideany(self, self.game.walls):
+            self.kill()
+        if pygame.time.get_ticks() - self.spawn_time > BULLET_LIFETIME:
+            self.kill()
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.wall_img
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, game, x, y, w, h):
+        self.groups = game.walls
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.rect = pygame.Rect(x, y, w, h)
+        self.hit_box = self.rect
+        self.x = x
+        self.y = y
+        self.rect.x = x
+        self.rect.y = y
